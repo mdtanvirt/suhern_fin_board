45de6e76cbfdee365b330d17cc4f9f70aae77694
tanvir <mdtanvirt@gmail.com>
2023-02-20 17:15:13 +0600

    Data exploration done

--- app.py
+++ app.py
@@ -1,24 +1,115 @@
 import streamlit as st
+import numpy as np
+import pandas as pd
 from streamlit_option_menu import option_menu
-st.title('This is for project initiation')
+from streamlit_toggle import st_toggle_switch
+from streamlit_extras.dataframe_explorer import dataframe_explorer
 
+st.set_page_config(layout="wide")
 
-# 1. as Side menu
-with st.sidebar:
-    selected = option_menu(
-        menu_title="Main Menu",
-        options=["Home", "Project", "Page"],
-        icons=["house", "book", "envelope"],
-        default_index=0,
-        orientation="horizontal",
-    )
+# Hide streamlit default menu and footer from the template
+hide_st_style = """
+    <style>
+    #MainMenu {visibility: hidden}
+    footer {visibility: hidden}
+    header {visibility: hidden}
+    div.block-container {padding-top:1rem;}
+    </style>
+"""
+st.markdown(hide_st_style, unsafe_allow_html=True)
 
+data = pd.read_excel("Dummy DataSet.xlsx")
 
-if selected == "Home":
-    st.title(f"You select {selected}")
+st.header('Financial Analysis')
 
-if selected == "Project":
-    st.title(f"You select {selected}")
+# Tab as page
+tab_dashboard, tab_raw_data, tab3 = st.tabs(["📈 Page1", "🗃 Page2", "🗃 Page3"])
+with st.container():
+    with st.sidebar:
+        st.subheader("Filter with metrix")
+        #nav_option = st.sidebar.radio("", ["Home", "Predection", "Contribute"])
+        #nav_option_toggle = st.write("Enable options")
+        nav_tog_op_1 = st_toggle_switch(
+            label="Item 1",
+            key="switch_1",
+            default_value=False,
+            label_after=False,
+            inactive_color="#D3D3D3",  # optional
+            active_color="#11567f",  # optional
+            track_color="#29B5E8",  # optional
+        )
+        nav_tog_op_2 = st_toggle_switch(
+            label="Item 2",
+            key="switch_2",
+            default_value=False,
+            label_after=False,
+            inactive_color="#D3D3D3",  # optional
+            active_color="#11567f",  # optional
+            track_color="#29B5E8",  # optional
+        )
+        nav_tog_op_3 = st_toggle_switch(
+            label="Item 3",
+            key="switch_3",
+            default_value=False,
+            label_after=False,
+            inactive_color="#D3D3D3",  # optional
+            active_color="#11567f",  # optional
+            track_color="#29B5E8",  # optional
+        )
+        nav_tog_op_4 = st_toggle_switch(
+            label="Item 4",
+            key="switch_4",
+            default_value=False,
+            label_after=False,
+            inactive_color="#D3D3D3",  # optional
+            active_color="#11567f",  # optional
+            track_color="#29B5E8",  # optional
+        )
+        nav_tog_op_5 = st_toggle_switch(
+            label="Item 5",
+            key="switch_6",
+            default_value=False,
+            label_after=False,
+            inactive_color="#D3D3D3",  # optional
+            active_color="#11567f",  # optional
+            track_color="#29B5E8",  # optional
+        )
+    
+    with tab_dashboard:
+        st.subheader("Summary")
+        col_barChart, col_pai_chart = st.columns(2)
 
-if selected == "Page":
-    st.title(f"You select {selected}")
+        with col_barChart:
+            st.write("This is Barchart")
+            st.success("Here Bar chart will be placeed")
+        
+        with col_pai_chart:
+            st.write("This is Pi Chart")
+            st.success("Here pi chart will be placeed")
+
+        st.subheader("Data Grid")
+        columns = ['Journal number', 'Date', 'Voucher', 'Posting type', 'Account entry description', 'Main account', 'Main account name', 'Amount', 'Amount in transaction currency', 'Currency', 'Party name']
+        new_df = data.filter(columns)
+        filtered_df = dataframe_explorer(new_df)
+        st.dataframe(new_df, use_container_width=True)
+
+    with tab_raw_data:
+        st.subheader("Raw data")
+        st.dataframe(data)
+    
+        @st.cache_data
+        def convert_df(data):
+            return data.to_csv().encode('utf-8')
+        
+        csv = convert_df(data)
+
+        st.download_button(
+            label="Download as CSV",
+            data=csv,
+            file_name='raw.csv',
+            mime='text/csv',
+        )
+
+    with tab3:
+        st.subheader("For page 3")
+        st.success("Page3 data analysis will be place here")
